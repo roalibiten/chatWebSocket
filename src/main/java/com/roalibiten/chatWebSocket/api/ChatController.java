49f08fcc00a6e0598d2d0d3b12f12b7a00dbd32f
roalibiten <46627141+roalibiten@users.noreply.github.com>
2021-08-05 17:26:00 +0300

    Initial commit

---
+++ src/main/java/com/roalibiten/chatWebSocket/api/ChatController.java
@@ -0,0 +1,35 @@
+package com.roalibiten.chatWebSocket.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin
+public class ChatController {
+
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+	
+	@MessageMapping("chat")
+	//@SendTo("/topic")
+	//@SendToUsers()
+	public void sendToUser(@Payload Message message) {
+		System.out.println(message);
+		messagingTemplate.convertAndSend("/topic/username",message);	
+	}
+	
+	@MessageMapping("toEmployee")
+	//@SendTo("/topic")
+	//@SendToUsers()
+	public void sendToEmployee(@Payload Message message) {
+		System.out.println(message);
+		messagingTemplate.convertAndSend("/topic",message);	
+	}
+	
+}
---
+++ src/test/java/com/roalibiten/chatWebSocket/ChatWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roalibiten.chatWebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
