66520f67cbcccb6f186e060b61a99d3ca629a974
roalibiten <46627141+roalibiten@users.noreply.github.com>
2021-08-06 03:10:32 +0300

    aaa

--- src/main/java/com/roalibiten/chatWebSocket/api/Message.java
+++ src/main/java/com/roalibiten/chatWebSocket/api/Message.java
@@ -3,8 +3,15 @@ package com.roalibiten.chatWebSocket.api;
 public class Message {
 	
 	private String sender;
+	public String getSendTo() {
+		return sendTo;
+	}
+	public void setSendTo(String sendTo) {
+		this.sendTo = sendTo;
+	}
 	private String message;
-	
+	private String sendTo;
+
 	public String getSender() {
 		return sender;
 	}
@@ -19,7 +26,7 @@ public class Message {
 	}
 	@Override
 	public String toString() {
-		return "Message [sender=" + sender + ", message=" + message + "]";
+		return "Message [sender=" + sender + ", message=" + message + ", sendTo=" + sendTo + "]";
 	}
 	
 	
