56fe5d8b3a44fdce764f78bf9c092b0290a71897
roalibiten <46627141+roalibiten@users.noreply.github.com>
2021-08-15 12:36:27 -0500

    databaseConnected

--- src/main/java/com/roalibiten/chatWebSocket/ChatWebSocketApplication.java
+++ src/main/java/com/roalibiten/chatWebSocket/ChatWebSocketApplication.java
@@ -3,6 +3,8 @@ package com.roalibiten.chatWebSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import database.DatabaseConnection;
+
 
 @SpringBootApplication
 
@@ -10,6 +12,8 @@ public class ChatWebSocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatWebSocketApplication.class, args);
+		DatabaseConnection aConnection=new DatabaseConnection();
+
 		
 	}
 
---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package database;
+
+
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseConnection {
+	
+
+	public DatabaseConnection() {
+		
+		 MongoClient client = MongoClients.create("mongodb+srv://roali:RoaliBiten@cluster0.rdbwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+	        MongoDatabase db = client.getDatabase("webChatApp");
+
+	        //MongoCollection col = db.getCollection("messages");
+
+	        //Document sampleDoc = new Document("_id", "1").append("name", "John Smith");
+
+	        //col.insertOne(sampleDoc);
+	
+	}
+	
+			
+}
